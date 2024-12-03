195d8fc2c38d42279dba8c7fd81cb38af8db1228
Vanduc250824 <leduc250824@gmail.com>
2024-12-03 10:43:30 +0700

    update connection

--- App_Coffee/App_Coffee/Program.cs
+++ App_Coffee/App_Coffee/Program.cs
@@ -1,3 +1,6 @@
+using App_Coffee.controller;
+using System.Data.SqlClient;
+
 namespace App_Coffee
 {
     internal static class Program
@@ -10,8 +13,13 @@ namespace App_Coffee
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            connection conn = new connection();
+            conn.OpenConnection();
+            conn.CloseConnection();
+
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new view.Form1());
+            Console.ReadLine();
         }
     }
 }
---
+++ App_Coffee/App_Coffee/controller/connection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Coffee.controller
+{
+    public class connection
+    {
+        private SqlConnection conn; 
+        private string connectionString;
+
+        
+        public connection()
+        {
+             string connectionString = "Data Source=Van_Duc25\\SQLEXPRESS;Initial Catalog=QLCOFFEE;Integrated Security=True;";
+             conn = new SqlConnection(connectionString);
+        }
+
+        // Phương thức để mở kết nối
+        public void OpenConnection()
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                    Console.WriteLine("Kết nối thành công!");
+                }
+                else
+                {
+                    Console.WriteLine("Kết nối đã mở sẵn.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi khi mở kết nối: " + ex.Message);
+            }
+        }
+
+
+        // Phương thức để đóng kết nối
+        public void CloseConnection()
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                    Console.WriteLine("Đóng kết nối thành công!");
+                }
+                else
+                {
+                    Console.WriteLine("Không có kết nối để đóng.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi khi đóng kết nối: " + ex.Message);
+            }
+        }
+
+    }
+}

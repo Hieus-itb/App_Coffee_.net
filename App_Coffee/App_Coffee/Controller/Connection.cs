704dfc2c4316bf7ce1af95f1a4c8a073647ffa35
Hieus-coder <vanhieu1524@gmail.com>
2024-12-03 09:14:53 +0700

    Create Project QLCOFFEE

---
+++ App_Coffee/App_Coffee/Controller/Connection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace App_Coffee.Controller
+{
+    class Connection
+    {
+        string conStr = @"Data Source=LAPTOP-EGMSJUVU\MAYHIEU;Initial Catalog=QLCOFFEE;Integrated Security=True;";
+        SqlConnection conn;
+        public Connection()
+        {
+            conn = new SqlConnection(conStr);
+        }
+        public DataSet LayDuLieu(string truyvan)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter(truyvan, conn);
+                da.Fill(ds);
+                return ds;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public bool ThucThi(string truyvan)
+        {
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(truyvan, conn);
+                int r = cmd.ExecuteNonQuery();
+                conn.Close();
+                return r > 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ App_Coffee/App_Coffee/view/Form1.cs
@@ -0,0 +1,10 @@
+namespace App_Coffee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

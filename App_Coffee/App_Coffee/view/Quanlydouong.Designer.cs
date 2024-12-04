d7a56b513e1cde642276eb7d544f73e8914590a4
Vanduc250824 <leduc250824@gmail.com>
2024-12-04 09:09:01 +0700

    up douong

--- App_Coffee/App_Coffee/view/Quanlydouong.Designer.cs
+++ App_Coffee/App_Coffee/view/Quanlydouong.Designer.cs
@@ -36,7 +36,23 @@
             btnDatmon = new Button();
             btnDatban = new Button();
             txtUser = new Label();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            btnThem = new Button();
+            btnSua = new Button();
+            btnXoa = new Button();
+            btnLuu = new Button();
+            btnKluu = new Button();
+            label5 = new Label();
+            textBox4 = new TextBox();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -116,17 +132,162 @@
             txtUser.TabIndex = 0;
             txtUser.Text = "Name";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(503, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(347, 41);
+            label1.TabIndex = 3;
+            label1.Text = "DANH SÁCH ĐỒ UỐNG";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(316, 329);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(668, 203);
+            dataGridView1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(316, 127);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Mã đồ uống:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(313, 190);
+            label3.Name = "label3";
+            label3.Size = new Size(95, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Tên đồ uống:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(314, 237);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Giá bán: ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(415, 124);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(44, 27);
+            textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(415, 187);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(328, 27);
+            textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(415, 234);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(163, 27);
+            textBox3.TabIndex = 10;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(314, 585);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(94, 29);
+            btnThem.TabIndex = 11;
+            btnThem.Text = "Thêm món";
+            btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            btnSua.Location = new Point(457, 585);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(94, 29);
+            btnSua.TabIndex = 12;
+            btnSua.Text = "Sửa món";
+            btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(603, 585);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(94, 29);
+            btnXoa.TabIndex = 13;
+            btnXoa.Text = "Xóa món";
+            btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnLuu
+            // 
+            btnLuu.Location = new Point(745, 585);
+            btnLuu.Name = "btnLuu";
+            btnLuu.Size = new Size(94, 29);
+            btnLuu.TabIndex = 14;
+            btnLuu.Text = "Lưu";
+            btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btnKluu
+            // 
+            btnKluu.Location = new Point(890, 585);
+            btnKluu.Name = "btnKluu";
+            btnKluu.Size = new Size(94, 29);
+            btnKluu.TabIndex = 15;
+            btnKluu.Text = "Không lưu";
+            btnKluu.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(316, 287);
+            label5.Name = "label5";
+            label5.Size = new Size(99, 20);
+            label5.TabIndex = 16;
+            label5.Text = "Giá nhập liệu:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(415, 284);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(163, 27);
+            textBox4.TabIndex = 17;
+            // 
             // Quanlydouong
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1140, 649);
+            ClientSize = new Size(1059, 649);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(btnKluu);
+            Controls.Add(btnLuu);
+            Controls.Add(btnXoa);
+            Controls.Add(btnSua);
+            Controls.Add(btnThem);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
             Controls.Add(panel1);
             Name = "Quanlydouong";
             Text = "Form1";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -139,5 +300,20 @@
         private Button btnDatmon;
         private Button btnDatban;
         private Label txtUser;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button btnThem;
+        private Button btnSua;
+        private Button btnXoa;
+        private Button btnLuu;
+        private Button btnKluu;
+        private Label label5;
+        private TextBox textBox4;
     }
 }
